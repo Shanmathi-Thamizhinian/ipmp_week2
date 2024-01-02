346144d0dc8ad1a50288fb44ccd4a458be7595f1
Shanmathi Thamizhinian <122631445+Shanmathi-Thamizhinian@users.noreply.github.com>
2024-01-02 16:10:41 +0530

    Create d0_q2.cpp

--- d0_q2.cpp
+++ d0_q2.cpp
@@ -16,7 +16,7 @@ class Solution
     bool checkKthBit(int n, int k)
     {
         int res=n>>k;
-        if(res%2==1) return true;
+        if(res%2==1) return true;  //if(res&1==1)
         else return false;
     }
 };
