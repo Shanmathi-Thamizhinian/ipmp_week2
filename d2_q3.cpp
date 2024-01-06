5db2b2cd7cb9c587cd24f04b34a79c9ccaeffcf2
Shanmathi Thamizhinian <122631445+Shanmathi-Thamizhinian@users.noreply.github.com>
2024-01-06 17:45:27 +0530

    Create d2_q3.cpp

---
+++ d2_q3.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+	public:
+   	int find(int n){
+   	    int sum=0;
+   	    for(int i=1;i<=n;i++){
+   	      sum=sum+i;
+   	      if(sum==n) return i;
+   	    }
+   	    return -1;
+   	}    
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		Solution ob;
+		int ans = ob.find(n);
+		cout << ans <<"\n";
+	}  
+	return 0;
+}
+// } Driver Code Ends
